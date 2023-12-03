1629564473e2b2034c92c7541a1ef8f5f0fcadb5
khyatisnbri <khyati@sbnri.com>
2023-12-04 02:49:56 +0530

    added row-limit and to-do list

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const rateLimit = require('express-rate-limit');
-const {Pool}  = require('pg');
+const ejs = require('ejs');
+const { Pool } = require('pg');
 
-const app = express()
-const port = 3000
+const date = require(__dirname + '/today.js');
+
+const app = express();
+const port = 3000;
 
 const pool = new Pool({
     user: 'postgres',
@@ -15,19 +18,53 @@ const pool = new Pool({
     password: 'postgre123',
     port: 5432,
 });
-``
-app.use(bodyParser.json());
 
-app.post('/register', async (req, res) => {
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+const items = [];
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute - 1ms * 1000 * 60
+    max: 5, // 5 requests per minute
+})
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: date.getDate(), items: items });
+});
+
+app.get("/register", (req, res) => {
+    res.render("register", { error: "" });
+});
+
+app.get("/login", (req, res) => {
+    res.render("login", { error: "" });
+});
+
+app.post("/work", (req, res) => {
+    const item = req.body.newItem;
+    items.push(item);
+    res.redirect("/work");
+});
+
+app.post('/register', limiter, async (req, res) => {
     const { username, email, password } = req.body;
+    console.log(req.body)
+    console.log(username, email, password);
 
     const userExists = await pool.query(`Select *
                                          from users 
                                          where username = $1
-                                         or email = $2 `, 
-                                         [username, email]);
+                                         or email = $2 `,
+        [username, email]);
     if (userExists.rows.length > 0) {
-        return res.status(400).json({ error: 'Username or Email already Registered'});
+        return res.render("register", { error: 'Username or Email already Registered' });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -35,21 +72,23 @@ app.post('/register', async (req, res) => {
     const result = await pool.query(`Insert into users
                                     (username, email, password)
                                     values ($1, $2, $3)
-                                    returning *`, 
-                                    [username, email, password]);
+                                    returning *`,
+        [username, email, hashedPassword]);
 
-    res.status(201).json({user: result.rows[0] });
+    res.redirect("/login");
+    // TODO: TO CREATE API ENDPOINT
+    // res.json({ token });
 });
 
-app.post('/login', async (req, res) => {
-    const {username, password} = req.body;
+app.post('/login', limiter, async (req, res) => {
+    const { username, password } = req.body;
 
     const result = await pool.query(`select * from users
                                     where username = $1`,
-                                    [username]);
+        [username]);
 
-    if (result.rows.length === 0){
-        return res.status(401).json({error: 'Invalid credentials, Please Try Again'});
+    if (result.rows.length === 0) {
+        return res.render("login", { error: 'Invalid credentials, Please Try Again' });
     }
 
     const user = result.rows[0];
@@ -57,27 +96,21 @@ app.post('/login', async (req, res) => {
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordMatch) {
-        return res.status(401).json({error: 'Invalid credentials, Please Try Again'});
+        return res.status(401).json({ error: 'Invalid credentials, Please Try Again' });
     }
 
-    const token = jwt.sign({userId: user.id}, 'Datiffy', {expiresIn: '1h'});
+    const token = jwt.sign({ userId: user.id }, 'Datiffy', { expiresIn: '1h' });
 
     await pool.query(`update users
                         set token = $1 
-                        where id = $2`, 
-                        [token, user.id]);
-    
-    res.json({token});
+                        where id = $2`,
+        [token, user.id]);
 
+    res.redirect("/work");
 });
 
-const limiter = rateLimit({
-    windowMs: 60 * 1000, // 1 minute - 1ms * 1000 * 60
-    max: 5, // 5 requests per minute
-})
-
 app.get('/secure-endpoint', limiter, (req, res) => {
-    res.json({message: 'This is a secure endpoint'});
+    res.json({ message: 'This is a secure endpoint' });
 });
 
 app.listen(port, () => {
